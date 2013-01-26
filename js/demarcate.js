9abbaae778fa2abd8b8e67d07256906252a4455e
William Hart <hart.wl@gmail.com>
2013-01-26 23:48:19 +0000

    Handle clicking off the editor or toolbar - saves

--- js/demarcate.js
+++ js/demarcate.js
@@ -182,13 +182,13 @@ function demarkdown(elem, ignore_extras) {
  */
 function generate_toolbar() {
     var toolbar = $("<div />", {id: 'demarcate_toolbar'});
-    toolbar.append($("<a />", {id: 'demarcate_h1', text: 'H1', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_h2', text: 'H2', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_h3', text: 'H3', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_h4', text: 'H4', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_h5', text: 'H5', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_h6', text: 'H6', href:"#" }));
-    toolbar.append($("<a />", {id: 'demarcate_p', text: 'P', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h1', class: 'demarcate_style', text: 'H1', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h2', class: 'demarcate_style', text: 'H2', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h3', class: 'demarcate_style', text: 'H3', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h4', class: 'demarcate_style', text: 'H4', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h5', class: 'demarcate_style', text: 'H5', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_h6', class: 'demarcate_style', text: 'H6', href:"#" }));
+    toolbar.append($("<a />", {id: 'demarcate_p', class: 'demarcate_style', text: 'P', href:"#" }));
     toolbar.append($("<a />", {id: 'demarcate_save', text: 'Save', href:"#" }));
     toolbar.append($("<a />", {id: 'demarcate_cancel', text: 'Cancel', href:"#" }));
     return toolbar;
@@ -242,6 +242,20 @@ function hide_editor(e) {
     current_demarcate_editor = null;
     current_demarcate_element.removeClass("demarcate_hide");
     current_demarcate_element = null;
+    $(document).unbind('mouseup', demarcate_click_elsewhere_save);
+}
+
+/* 
+ * A mouseup handler that checks if the toolbar or editor was clicked.
+ * If not, it hides the editor.  Implemented as a separate function so
+ * it can be unbound.
+ */
+function demarcate_click_elsewhere_save(e) {
+    var tb = $("#demarcate_toolbar");
+    if (! current_demarcate_editor.is(e.target) && 
+        ! tb.is(e.target) && tb.has(e.target).length === 0) {
+        $("#demarcate_save").click();
+    }
 }
 
 /* 
@@ -249,11 +263,15 @@ function hide_editor(e) {
  */
 function restore_demarcate_toolbar_handlers() {
 
+    // handle hitting the return key inside the editor - saves it
     current_demarcate_editor.keydown(function(key) {
         if (key.keyCode == 13) {
             $("#demarcate_save").click();
         }
     });
+
+    // handle clicking outside the div
+    $(document).bind('mouseup', demarcate_click_elsewhere_save);
 
     // cancel button
     $("#demarcate_cancel").on('click', function(e) {
@@ -291,7 +309,7 @@ function restore_demarcate_toolbar_handlers() {
         len = editor_whitelist.length
         for (var i = 0; i < len; i++) {
             live_selector = "#" + this.attr('id') + " " + editor_whitelist[i];
-            $(live_selector).on('dblclick', function() {
+            $(live_selector).on('click', function() {
                 display_editor(this);
             });
         }
