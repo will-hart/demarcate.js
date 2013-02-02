bbd9cb1dfa4501c6b134da7b40b2eb8ca1c80fe6
William Hart <hart.wl@gmail.com>
2013-02-02 21:50:15 +0000

    Improve deployment script for generating minified dependencies

--- deploy.py
+++ deploy.py
@@ -11,21 +11,38 @@ a GPLv3 license from
 
 """
 from jsmin import jsmin
+from cssmin import cssmin
 
 if __name__ == "__main__":
     op = ""
 
     # put in showdown dependency
-    print "Minifying Showdown Dependency"
+    print "\n\n\nBuilding demarcate.js Dependencies:\n\nGenerating minified javascripts"
+    print "   >> Minifying Showdown Dependency"
     with open("js/showdown.min.js","r") as sd:
         op = jsmin(sd.read())
 
-    print "Minifying Demarcate Library"
+    print "   >> Minifying jQuery.autosize Dependency"
+    with open("js/jquery.autosize.js","r") as ats:
+        op = jsmin(ats.read())
+
+    with open("js/showdown.min.js","r") as sd:
+        op = jsmin(sd.read())
+
+    print "   >> Minifying Demarcate Library"
     with open("js/demarcate.js", "r") as dm:
         op += jsmin(dm.read())
 
-    print "Writing minified text to js/demarcate.min.js"
+    print " <<   Writing minified text to js/demarcate.min.js"
     with open("js/demarcate.min.js", "w") as dm_min:
         dm_min.write(op)
 
-    print "All done!"
+    print "Javascript complete.\n\nMoving on to CSS\n   >> Minifying demarcate.css"
+    with open("css/demarcate.css", "r") as cs:
+        op = cssmin(cs.read())
+
+    print " <<   Writing demarcate.min.css"
+    with open("css/demarcate.min.css", "w") as cs_min:
+        cs_min.write(op)
+
+    print "\nAll done!\n\n"
