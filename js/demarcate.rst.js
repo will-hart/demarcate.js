b46cbd40d6cff69b16eaf017f2d82c8429cf5e80
William Hart <hart.wl@gmail.com>
2013-02-15 14:25:53 +0000

    Add skeleton plugin for demaracte rst exporT

---
+++ js/demarcate.rst.js
@@ -0,0 +1,12 @@
+/*************************************************************************
+*      DemarcateJS is an in-place Markdown editor and decoder            *
+*                                                                        *
+*      This is a RestructuredText plugin that allows demarcate.js to     *
+*      generate RestructuredText from HTML.                              *
+*                                                                        *
+*      It supports demarcate.js 1.1.5 or greater                         *
+*                                                                        *
+*************************************************************************/
+
+/* Add an entry point which generates RST from HTML */
+demarcate.rst
